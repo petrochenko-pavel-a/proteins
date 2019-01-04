b1d452d133fc1260c68fd7c4cff6bc9fbf98e21f
petrochenko pavel <pavel@onpositive.com>
2019-01-04 17:16:24 +0700

    splitter should read data from file now.

--- split_data.py
+++ split_data.py
@@ -38,29 +38,41 @@ def split(X,y):
     testSet=set()
 
     np.random.seed(12)
-    for v in zz:
-        # now we should start choosing examples
-        goodClasss = np.where(y[:, v] > 0)[0]
-        np.random.shuffle(goodClasss)
-        test=0
-        train=0
-        for c in goodClasss:
-
-            if test*5<train:
-                if not c in trainSet:
-                    testSet.add(c)
-                    test=test+1
-                else:
-                    train=train+1
-                    trainSet.add(c);
+    if os.path.exists("train.txt"):
+        with open("train.txt","r") as f:
+            trainLines=[x.strip() for x in f.readlines()]
+        with open("test.txt","r") as f:
+            testLines=[x.strip() for x in f.readlines()]
+            #just for debug
+        for v in range(y.shape[0]):
+            if X[v] in trainLines:
+                trainSet.add(v)
             else:
-                if not c in testSet:
-                    train = train + 1
-                    trainSet.add(c);
+                testSet.add(v)
+    else:
+        for v in zz:
+            # now we should start choosing examples
+            goodClasss = np.where(y[:, v] > 0)[0]
+            np.random.shuffle(goodClasss)
+            test=0
+            train=0
+            for c in goodClasss:
+
+                if test*5<train:
+                    if not c in trainSet:
+                        testSet.add(c)
+                        test=test+1
+                    else:
+                        train=train+1
+                        trainSet.add(c);
                 else:
-                    testSet.add(c)
-                    test = test + 1
-        print(test,train)
+                    if not c in testSet:
+                        train = train + 1
+                        trainSet.add(c);
+                    else:
+                        testSet.add(c)
+                        test = test + 1
+            print(test,train)
         # vv=goodClasss
         # vv1=vv[:vv.shape[0]//5]
         # vv2=vv[vv.shape[0]//5:]
